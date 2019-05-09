bdb9059b52736cc0c3bb3a714cf9618dbcfcdbb6
澤田洋二 <yoji@MacBook-Pro.local>
2019-05-09 12:06:39 +0900

    #cms-articlesまで

---
+++ src/Model/Entity/Article.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class Articles extends Entity
+{
+	protected $_accessible = [
+		'*' => true,
+		'id' => false,
+		'slug' => false,
+  	];
+}
---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class ArticlesTable extends Table
+{
+	public function initialize(array $config)
+	{
+		$this->addBehaior('Timestamp');
+	}
+}
